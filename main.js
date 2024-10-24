8f7e1d4158727f6f359dd353069db300be0965eb
Sofwhee <sofiamsola@outlook.com>
2024-10-24 17:21:36 -0400

    correct syntax errors in html and js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { createIconElement, createIconElements } from "./modules/createIconElements.js";
+import { createIconImgElement, createIconElements } from "./modules/createIconElements.js";
 import { animDelayThruDom } from "./modules/animDelayThruDom.mjs";
 // import { childElementsByClass } from "./modules/childElementsByClass.js";
 
@@ -6,6 +6,7 @@ import { animDelayThruDom } from "./modules/animDelayThruDom.mjs";
 // change these vars to suit needs
 const iconsClass = "langIcon upSpawn"
 const iconsHeight = "80px"
+const iconsDestination = document.getElementById("langContainer")
 // add icon svg link to list and it will appear on site in order
 const iconLinks = [
   "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
@@ -16,26 +17,19 @@ const iconLinks = [
   "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg",
   "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
 ]
-
-// make icon elements and apply them to page
-// (don't change these)
+// make icon elements and apply to destination
 const iconsArray_ = Array.from(iconLinks)
-const iconsDestination = document.getElementById("langContainer")
 const iconElements = createIconElements(iconsArray_, iconsClass, iconsHeight)
 for (const iconElement of iconElements) {
   iconsDestination.appendChild(iconElement)
 }
-// ---
+
 
 // create a rolling animation delay from parent to children based on class
-// change these var to suit needs
+// change these vars to suit needs
 let elementParent = document.getElementById("langContainer")
 let animationName = "upSpawn" 
 let startingDelay = 0.8
 let delayInterval = 0.2
-
-
-// apply delay (don't change these)
+// apply
 animDelayThruDom(iconElements, startingDelay, delayInterval)
-// ---
-
